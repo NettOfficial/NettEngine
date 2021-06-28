7760b9e153c391295513d0b9f322bc067e024de1
Mykola Kokoulin <mykola.kokoulin@gmail.com>
2021-06-28 17:01:19 +0200

    Some changes in log

--- NettEngine/src/NettEngine/Log.h
+++ NettEngine/src/NettEngine/Log.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "Core.h"
-#include "spdlog/spdlog.h"
-#include "spdlog/fmt/ostr.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 
 namespace NettEngine {
 
@@ -26,11 +26,11 @@ namespace NettEngine {
 #define NE_CORE_INFO(...)       ::NettEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define NE_CORE_WARN(...)       ::NettEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define NE_CORE_ERROR(...)      ::NettEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define NE_CORE_FATAL(...)      ::NettEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+//#define NE_CORE_FATAL(...)      ::NettEngine::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 //Client log macros
 #define NE_TRACE(...)      ::NettEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define NE_INFO(...)       ::NettEngine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define NE_WARN(...)       ::NettEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define NE_ERROR(...)      ::NettEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define NE_FATAL(...)      ::NettEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
+//#define NE_FATAL(...)      ::NettEngine::Log::GetClientLogger()->fatal(__VA_ARGS__)
